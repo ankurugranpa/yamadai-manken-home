aaa3cdbb08962c5c6d5edb2a6dadecd2fd5f0e3f
ankuru <ankuru.ahahahaha@gmail.com>
2025-09-02 13:41:00 +0900

    add: shadcnのドロワー追加

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -11,6 +11,7 @@ const Drawer = ({
 }) => (
   <DrawerPrimitive.Root
     shouldScaleBackground={shouldScaleBackground}
+    preventScrollRestoration={false}
     {...props}
   />
 )
@@ -28,7 +29,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-50 bg-black/80", className)}
+    className={cn("fixed inset-0 z-[100] bg-black/80", className)}
     {...props}
   />
 ))
@@ -43,12 +44,12 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-[101] mt-24 flex h-auto flex-col rounded-t-[10px] border",
         className
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-gray-300" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
