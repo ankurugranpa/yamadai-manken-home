579f1f931ef43bc82c848623462de226188eea14
ankuru <ankuru.ahahahaha@gmail.com>
2025-09-03 16:23:32 +0900

    fix: タッチの範囲を画面の描写領域全体に適用

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -155,12 +155,16 @@ const CarouselContent = React.forwardRef<
   const { carouselRef, orientation } = useCarousel()
 
   return (
-    <div ref={carouselRef} className="overflow-hidden">
+    <div 
+      ref={carouselRef} 
+      className="overflow-hidden h-full w-full"
+      style={{ touchAction: 'pan-x' }}
+    >
       <div
         ref={ref}
         className={cn(
-          "flex",
-          orientation === "horizontal" ? "-ml-4" : "-mt-4 flex-col",
+          "flex h-full",
+          orientation === "horizontal" ? "" : "flex-col",
           className
         )}
         {...props}
@@ -182,8 +186,7 @@ const CarouselItem = React.forwardRef<
       role="group"
       aria-roledescription="slide"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        "min-w-0 shrink-0 grow-0 basis-full h-full",
         className
       )}
       {...props}
