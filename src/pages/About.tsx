33495fbe37ab170f07e90a1e1dac88d9171b9c32
ankuru <ankuru.ahahahaha@gmail.com>
2025-09-07 02:15:36 +0900

    feat: 本棚ページの仮実装

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,23 @@
+import toLoveruAnkuru from '../assets/to_loveru_ankuru.png';
+import mato_19 from '../assets/19.png';
+import b5 from '../assets/b5.png';
+import karaage from '../assets/karaage.png'
+
 export function About() {
   return (
-    <div className="h-full flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-3xl font-bold text-gray-900">概要</h1>
-        <p className="mt-4 text-gray-600">左タップ・右スワイプで次へ<br />右タップ・左スワイプで前へ</p>
-      </div>
+    <div className="h-full w-full flex items-center justify-center">
+      <img 
+        src={karaage} 
+        alt="To Love-Ru Ankuru" 
+        className="max-w-full max-h-full object-contain"
+      />
+      {/* <div className="flex-1 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold text-gray-900">ホーム</h1>
+          <p className="mt-4 text-gray-600">左タップ・右スワイプで次へ<br />右タップ・左スワイプで前へ</p>
+        </div>
+      </div>  
+      */}
     </div>
   );
 }
---
+++ src/pages/Book1Page2.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Book1Page2() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <img 
+        src="/src/assets/to_loveru_line.svg"
+        alt="サンプル本1 - ページ2" 
+        className="max-w-full max-h-full object-contain"
+      />
+    </div>
+  );
+}
