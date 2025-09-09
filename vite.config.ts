c42f052d306d1be6f4aee25f8ceec9c78df43d6e
ankuru <ankuru.ahahahaha@gmail.com>
2025-09-10 02:40:26 +0900

    fix: baseの修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'yamadai-manken-home',
+  base: '/yamadai-manken-home/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
