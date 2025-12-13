17d58c090be2bd632e740cc19af3f36285f1105d
ankuru <ankuru.ahahahaha@gmail.com>
2025-12-13 13:20:06 +0900

    Revert "feat: apiの作成"
    This reverts commit ad53eabf3400c144805ebe813e2b2f4109d2c8df.

--- api/usecases/listWorkPages.ts
+++
@@ -1,9 +0,0 @@
-import type { WorksRepository } from "../repositories/worksRepository.js";
-import type { WorkPagesResponse } from "../types/index.js";
-
-export const listWorkPages = async (
-  worksRepo: WorksRepository,
-  workId: string,
-): Promise<WorkPagesResponse> => {
-  return worksRepo.listPages(workId);
-};
